9d0e232808184ffe23fb6842451d5ec455dd33c8
Luan Alves Tanikawa <118451859+luanTanikawa@users.noreply.github.com>
2023-06-15 22:40:19 -0300

    Atualização FINALMENTE

--- script.js
+++ script.js
@@ -19,31 +19,39 @@ beneficiosBtns.map((e) => {
 
 duvidasBtns.map((e) => {
     e.addEventListener('click', () => {
-        let paragrafo = e.nextElementSibling
+        let botao = e.parentElement
         let img = e.children[1]
+        let paragrafo = e.nextElementSibling
 
-        if (paragrafo.classList.contains('mostrar') == false) {
+        let alturaParagrafo = paragrafo.offsetHeight
+
+
+        if (botao.classList.contains('mostrar') == false) {
             duvidasBtns.map((k) => {
-                let paragrafo = k.nextElementSibling
+                let botao = k.parentElement
                 let img = k.children[1]
                 
-                if (paragrafo.classList.contains('mostrar')) {
-                    paragrafo.classList.remove('mostrar')
+                if (botao.classList.contains('mostrar')) {
+                    botao.classList.remove('mostrar')
+                    botao.style.height = `${55}px`
                     img.classList.remove('girar')
                 }
             })
         }
 
-        if (paragrafo.classList.contains('mostrar')) {
-            paragrafo.classList.remove('mostrar')
+        if (botao.classList.contains('mostrar')) {
+            botao.classList.remove('mostrar')
+            botao.style.height = `${55}px`
         } else {
-            paragrafo.classList.add('mostrar')
+            botao.classList.add('mostrar')
+            botao.style.height = `${alturaParagrafo + 55}px`
         }
         
-        if (paragrafo.classList.contains('mostrar')) {
+        if (botao.classList.contains('mostrar')) {
             img.classList.add('girar')
         } else {
             img.classList.remove('girar')
         }
     })
 })
+
